c06d8e85c308533ca9116725db0a9d0d8e189f79
hyphast <oshcherbakov2018@mail.ru>
2021-09-27 21:40:31 +0400

    fix

--- controllers/adminContollers/appointmentController.js
+++ controllers/adminContollers/appointmentController.js
@@ -1,4 +1,4 @@
-const AppointmentService = require('../../services/adminServices/appointments/appointmentService');
+const AppointmentService = require('../../services/appointmentService');
 
 class AppointmentController {
   async createAppointment(req, res, next) {
@@ -35,44 +35,13 @@ class AppointmentController {
     }
   }
 
-  async getAppointments(req, res, next) {
+  async getCreatedAppointments(req, res, next) {
     try {
-      const filter = req.query.filter ? JSON.parse(req.query.filter) : null;
-      const range = req.query.range ? JSON.parse(req.query.range) : null;
-      const sort = req.query.sort ? JSON.parse(req.query.sort) : null;
+      const appointments = await AppointmentService.getCreatedAppointments();
 
-      const appointments = await AppointmentService.getAppointments(filter, range, sort);
-
-      return res.set('Content-Range', appointments.countDocuments.toString()).json(appointments.appointments);
+      return res.json(appointments);
     } catch (e) {
       next(e);
-    }
-  }
-
-  async getOneAppointment(req, res, next) {
-    try {
-      const id = req.params.id;
-
-      const appointment = await AppointmentService.getOneAppointment(id);
-
-      return res.json(appointment);
-    } catch (e) {
-      next(e);
-    }
-  }
-
-  async updateOneAppointment(req, res, next) {
-    try {
-      const id = req.params.id;
-      const {appointments, date} = req.body;
-      console.log('appointments', appointments)
-
-
-      const appointment = await AppointmentService.updateOneAppointment(id, appointments, date);
-
-      res.json(appointment);
-    } catch (e) {
-      next(e)
     }
   }
 }
