657b50542aa3e924f5a5320c240801ac39fe8abc
hyphast <oshcherbakov2018@mail.ru>
2021-09-27 21:49:20 +0400

    fix

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
  if (process.env.NODE_ENV === 'production') {
-   app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')));
+   app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
    })
  }
 
