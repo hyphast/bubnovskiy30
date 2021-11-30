3a4879ca524f0a9543aecbe4529cf40429435861
hyphast <oshcherbakov2018@mail.ru>
2021-11-30 22:51:11 +0400

    :recycle: Refactor common InputController component to Typescript

---
+++ client/src/components/Auth/Common/InputController/InputController.tsx
@@ -0,0 +1,39 @@
+import React, {FC} from 'react';
+import {Form, Input} from "antd";
+import FormError from "../Error/FormError";
+import {useController} from "react-hook-form";
+
+type PropsType = {
+  errors: any,
+  field: string,
+  control: any,
+  placeholder: string,
+  inputPassword?: boolean,
+  type?: string,
+  className?: string,
+  prefix?: React.ReactNode,
+}
+
+const InputController: FC<PropsType> = ({errors, field, control, ...restProps}) => {
+  const {field: input} = useController({name: field, control});
+  const {inputPassword, ...rest} = restProps;
+  return (
+    <Form.Item validateStatus={errors[field] ? "error" : ""} hasFeedback>
+      {inputPassword
+        ?
+        <Input.Password {...input} {...rest}  />
+        :
+        <Input {...input} {...restProps}/>
+      }
+      <FormError errors={errors} field={field} />
+    </Form.Item>
+  );
+};
+
+InputController.defaultProps = {
+  errors: {},
+  field: '',
+  control: {},
+}
+
+export default InputController;
