30ec1747334a9d9a0f3e39be9eb3893a1cf4197a
hyphast <oshcherbakov2018@mail.ru>
2021-11-30 22:48:14 +0400

    :recycle: Refactor common FormError component to Typescript

---
+++ client/src/components/Auth/Common/Error/FormError.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+import './Error.scss';
+
+type PropsType = {
+  errors: any,
+  field: string,
+}
+
+const FormError: FC<PropsType> = ({errors, field}) => {
+  return (
+    <>
+        {errors[field] && <span className='error'>{errors[field].message}</span>}
+    </>
+  );
+};
+
+FormError.defaultProps = {
+  errors: {},
+  field: '',
+}
+
+export default FormError;
